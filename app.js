3a312b745b5e287efeb3d050a628cc41a92fc184
Andrey <arkadij1998@gmail.com>
2018-02-18 18:26:05 +0300

    examples were added

---
+++ app.js
@@ -0,0 +1,42 @@
+/*
+var button = document.querySelector('button');
+
+button.addEventListener('click', function (e) {
+  e.preventDefault();
+});
+
+var input = document.querySelector('input');
+
+input.addEventListener('input', function (e) {
+  console.log(e.target.value);
+});
+*/
+
+/*
+window.addEventListener('mousemove', function (e) {
+  console.log(e.clientY, e.clientX);
+  console.log(e.screenY, e.screenX);
+});
+*/
+
+var outer = document.querySelector('.outer');
+
+function onInnerBlockClick(e) {
+  e.target.classList.toggle('red');
+  console.log(e.currentTarget);
+}
+
+outer.addEventListener('click', onInnerBlockClick);
+
+/*
+for (var i = 0; i < innerBlocks.length; i++) {
+  innerBlocks[i].addEventListener('click', onInnerBlockClick);
+}
+*/
+
+var form = document.querySelector('.form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log('submit');
+});
