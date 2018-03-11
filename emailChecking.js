f4d53e985f95dae809cb85bd890842a4d071d3be
Andrey <arkadij1998@gmail.com>
2018-03-11 17:06:49 +0300

    Доработки паттерна

--- emailChecking.js
+++ emailChecking.js
@@ -1,8 +1,9 @@
 var email = document.querySelector('#email');
 
+var EMAIL_PATTERN = /^[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z0-9]+$/;
+
 email.addEventListener('change', function(e) {
-  var re = /\S+@\S+\.\S+/
-  if (re.test(e.target.value)) {
+  if (EMAIL_PATTERN.test(e.target.value)) {
     e.target.setAttribute('style', 'border: 2px solid green');
   } else {
     e.target.setAttribute('style', 'border: 2px solid red');
